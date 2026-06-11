567de155df04c72b52a5814a0a1070d34990cb3e
ss <sander@sonstabo.com>
2026-06-11 07:15:46 +0200

    chore: remove jsr publishing

--- jsr/mod.ts
+++
@@ -1,7 +0,0 @@
-export const name = "runtime-checker";
-export const version = "1.0.2";
-export const repository = "https://github.com/usebarekey/runtime-checker";
-
-export function install_command(): string {
-  return "cargo install runtime-checker";
-}
